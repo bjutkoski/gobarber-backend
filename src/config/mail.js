8e4d467adc1225a0f19b211ff7f5c45258c01a56
Bruno Jutkoski <bjutkoski@MacBook-Pro-de-Bruno.local>
2019-09-30 23:40:55 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '43f078e0210c00',
+    pass: 'bf6d66bd39830a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
